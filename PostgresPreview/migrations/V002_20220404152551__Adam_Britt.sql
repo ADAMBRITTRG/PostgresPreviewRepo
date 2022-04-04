b0e6ca62d7651e8d98c433144f7f54af729e3135
Adam Britt <adam.britt@red-gate.com>
2022-04-04 16:58:44 +0100

    MV3

---
+++ PostgresPreview/migrations/V002_20220404152551__Adam_Britt.sql
@@ -0,0 +1,8 @@
+SET check_function_bodies = false;
+
+
+DO language plpgsql $$BEGIN RAISE NOTICE 'Creating public.v31...';END$$;
+CREATE TABLE public.v31 (
+    
+);
+SET check_function_bodies = true;
