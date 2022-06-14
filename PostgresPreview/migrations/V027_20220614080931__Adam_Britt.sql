279c44ae9bb4016648611f3337c0fc0ffdb885a5
Adam Britt <adam.britt@red-gate.com>
2022-06-14 09:19:31 +0100

    comment out test schema

--- PostgresPreview/migrations/V027_20220614080931__Adam_Britt.sql
+++ PostgresPreview/migrations/V027_20220614080931__Adam_Britt.sql
@@ -1,12 +1,12 @@
 SET check_function_bodies = false;
 
 
-DO language plpgsql $$BEGIN RAISE NOTICE 'Dropping "Test"."TestTableInTestSchema"';END$$;
-DROP TABLE "Test"."TestTableInTestSchema";
+--DO language plpgsql $$BEGIN RAISE NOTICE 'Dropping "Test"."TestTableInTestSchema"';END$$;
+--DROP TABLE "Test"."TestTableInTestSchema";
 
 
-DO language plpgsql $$BEGIN RAISE NOTICE 'Dropping "Test"';END$$;
-DROP SCHEMA "Test";
+--DO language plpgsql $$BEGIN RAISE NOTICE 'Dropping "Test"';END$$;
+--DROP SCHEMA "Test";
 
 
 DO language plpgsql $$BEGIN RAISE NOTICE 'Altering id...';END$$;
